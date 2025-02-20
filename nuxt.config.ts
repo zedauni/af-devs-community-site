62c4cf0a040f467a76c1f98b441bf519aaf64279
zedauni <11563693+zedauni@users.noreply.github.com>
2025-02-20 22:12:49 +0100

    Added packages + configs : nuxt/icon, vueuse and nuxtjs/color-mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,4 +8,32 @@ export default defineNuxtConfig({
       tailwindcss(),
     ],
   },
+  modules: ['@nuxtjs/color-mode', '@nuxt/icon', '@vueuse/nuxt'],
+  colorMode: {
+    preference: 'system', // default value of $colorMode.preference
+    fallback: 'light', // fallback value if not system preference found
+    hid: 'nuxt-color-mode-script',
+    globalName: '__NUXT_COLOR_MODE__',
+    componentName: 'ColorScheme',
+    classPrefix: '',
+    classSuffix: '',
+    dataValue: 'theme',
+    storage: 'cookie', // 'localStorage' or 'sessionStorage' or 'cookie'
+    storageKey: 'app-color-mode'
+  },
+  icon: {
+    clientBundle: {
+      // list of icons to explicitly include in the client bundle
+      icons: [
+        'ri:moon-clear-line',
+        'ri:sun-fill',
+        'ri:computer-line',
+      ],
+      // scan this folders for icons to include in the client bundle
+      scan: {
+        globInclude: ['components/**/*.vue', 'layout/**/*.vue', 'pages/**/*.vue'],
+        globExclude: ['node_modules', 'dist'],
+      },
+    },
+  },
 })
