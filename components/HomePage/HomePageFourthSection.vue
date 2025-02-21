1c520aa4083eb88b0ff39d48f316ad24ccf52ef4
zedauni <11563693+zedauni@users.noreply.github.com>
2025-02-21 14:33:34 +0100

    Added latest sections

---
+++ components/HomePage/HomePageFourthSection.vue
@@ -0,0 +1,74 @@
+<template>
+  <section class="px-24 mt-32 max-md:mt-10 rounded-none" aria-labelledby="business-community-title">
+    <div class="flex gap-5 max-md:flex-col">
+      <div class="flex flex-col relative grow justify-center items-center w-6/12 max-md:ml-0 max-md:w-full">
+        <img
+          loading="lazy"
+          src="~/assets/img/girl-on-desk.png"
+          alt="Business network illustration"
+          class="object-cover absolute size-full self-start mt-0 max-w-full rounded-[81px] max-md:mt-0"
+        />
+        <div
+          class="flex relative flex-col justify-end w-full h-full max-w-full"
+        >
+          <div
+            class="self-end -mb-10 -mr-[50%] w-11/12 px-12 py-10 max-w-full rounded-3xl border border-solid backdrop-blur-xl bg-black/30 border-white/30 max-md:px-5"
+          >
+            <div class="flex gap-5 max-md:flex-col">
+              <div class="flex flex-col max-md:ml-0 max-md:w-full">
+                <div class="flex flex-col text-white max-md:mt-7">
+                  <div
+                    class="flex gap-10 items-center"
+                  >
+                    <div
+                      class="flex items-center justify-center"
+                      role="img"
+                      aria-label="Decorative icon"
+                    >
+                      <VectorPartnerExchange class="w-16 h-16 fill-ora" />
+                    </div>
+                    <h3 class="grow font-medium text-4xl text-white">
+                      Let’s build meaningful business
+                    </h3>
+                  </div>
+                  <p class="mt-10 leading-8 text-white">
+                    The ultimate platform for professionals &amp; businesses to connect,
+                    collaborate, and grow together. Whether you're looking to expand your
+                    network or find new opportunities, our community is here to support you.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="flex flex-col ml-5 w-6/12 max-md:ml-0 max-md:w-full">
+        <div class="flex flex-col max-w-full w-full">
+          <h1
+            class="self-start text-7xl font-semibold tracking-tighter text-white leading-[73px] max-md:max-w-full max-md:text-4xl max-md:leading-12"
+          >
+            Your business
+            <span class="text-ora">network &amp; growth</span> community
+          </h1>
+          <p
+            class="mt-20 text-xl font-medium leading-8 text-white text-opacity-80 max-md:mt-10 max-md:max-w-full"
+          >
+            With features like group discussions, business matchmaking, and
+            expert-led forums, you'll have access to valuable resources.
+          </p>
+        </div>
+        <div
+          class="flex flex-col self-end mt-10 -mb-10 p-14 max-w-full rounded-3xl border border-solid bg-transparent border-white/30 w-xs max-md:px-5"
+        >
+          <div
+            class="text-9xl text-ora max-md:text-4xl font-manrope"
+            aria-label="79 plus professionals connected"
+          >
+            79+
+          </div>
+          <div class="mt-5 text-4xl text-white">Professionals connect</div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
---
+++ components/HomePage/HomePageThirdSection.vue
@@ -0,0 +1,117 @@
+<template>
+  <section
+    class="flex flex-col px-24 mt-32 max-md:mt-10 rounded-none"
+    aria-labelledby="business-community-title"
+  >
+    <div class="z-10 w-full max-md:max-w-full">
+      <div class="flex gap-5 max-md:flex-col">
+        <div class="flex flex-col w-4/12 max-md:ml-0 max-md:w-full">
+          <div class="flex flex-col max-md:mt-10 max-md:max-w-full">
+            <h1
+              id="business-community-title"
+              class="text-5xl font-semibold tracking-tighter text-white leading-14 max-md:mr-2.5 max-md:max-w-full max-md:text-4xl max-md:leading-12 font-manrope"
+            >
+              Business and <span class="text-ora">community</span> come
+              together.
+            </h1>
+            <p
+              class="mt-20 leading-8 text-white text-opacity-80 max-md:mt-10 max-md:max-w-full"
+            >
+              With features like group discussions, business matchmaking, and
+              expert-led forums, you'll have access to the tools and resources
+              to elevate your career or business.
+            </p>
+          </div>
+        </div>
+        <div
+          class="flex flex-col justify-between ml-5 w-4/12 max-md:ml-0 max-md:w-full"
+        >
+          <div class="flex flex-col grow text-white max-md:mt-10">
+            <VectorLayers class="mb-3" />
+            <h2 class="text-4xl">Let's build meaningful business</h2>
+            <p class="mt-8 leading-8 text-white text-opacity-80">
+              With features like group discussions, business matchmaking, and
+              expert-led forums, you'll have access
+            </p>
+            <a
+              href="#"
+              class="self-start px-8 py-3 mt-10 font-semibold text-center border border-solid border-white/40 rounded-full max-md:px-5"
+              aria-label="Read more about building meaningful business"
+            >
+              Read More
+            </a>
+          </div>
+        </div>
+        <div
+          class="flex flex-col justify-between ml-5 w-4/12 max-md:ml-0 max-md:w-full"
+        >
+          <div class="flex flex-col grow items-start text-white max-md:mt-10">
+            <VectorMoveUp class="mb-3" />
+            <h2 class="text-4xl">Connections and expand</h2>
+            <p class="self-stretch mt-8 leading-8 text-white text-opacity-80">
+              With features like group discussions, business matchmaking, and
+              expert-led forums, you'll have access
+            </p>
+            <a
+              href="#"
+              class="px-8 py-3 mt-10 font-semibold text-center border border-solid border-white/40 rounded-full max-md:px-5"
+              aria-label="Read more about connections and expansion"
+            >
+              Read More
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="mt-24 w-full max-md:mt-10 max-md:max-w-full">
+      <div class="flex gap-5 max-md:flex-col">
+        <div class="flex flex-col w-4/12 max-md:ml-0 max-md:w-full">
+          <div
+            class="flex grow items-center px-5 py-5 w-full text-2xl text-white rounded-2xl border border-solid bg-transparent border-white/30 max-md:px-5 max-md:mt-10 max-md:max-w-full"
+            tabindex="0"
+            role="region"
+            aria-label="Platform that takes your professional"
+          >
+            <div class="flex w-16 justify-center mr-4">
+              <VectorLda />
+            </div>
+            Platform that takes your professional
+          </div>
+        </div>
+        <div class="flex flex-col ml-5 w-4/12 max-md:ml-0 max-md:w-full">
+          <div
+            class="flex grow items-center px-5 py-5 w-full text-2xl text-white rounded-2xl border border-solid bg-transparent border-white/30 max-md:px-5 max-md:mt-10 max-md:max-w-full"
+            tabindex="0"
+            role="region"
+            aria-label="Connect with industry leaders & innovators"
+          >
+            <div class="flex w-16 justify-center mr-4">
+              <VectorMotionSensorActive />
+            </div>
+            Connect with industry leaders & innovators
+          </div>
+        </div>
+        <div class="flex flex-col ml-5 w-4/12 max-md:ml-0 max-md:w-full">
+          <div
+            class="flex grow items-center px-5 py-5 w-full text-2xl text-white rounded-2xl border border-solid bg-transparent border-white/30 max-md:px-5 max-md:mt-10 max-md:max-w-full"
+            tabindex="0"
+            role="region"
+            aria-label="Opportunities meet connections"
+          >
+            <div class="flex w-16 justify-center mr-4">
+              <VectorMatter />
+            </div>
+            Opportunities meet connections
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script lang="ts" setup>
+import { VectorLayers, VectorMatter } from "#components";
+import VectorMotionSensorActive from "../Vector/VectorMotionSensorActive.vue";
+</script>
+
+<style></style>
